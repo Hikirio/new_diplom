f846046a80ce05420f75e90c73e13622042dd416
anatoliy <anatoliy.matvienkov38@gmail.com>
2019-05-23 15:41:43 +0300

    make routing

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,64 @@
 |
 */
 
+use App\Vacancy;
+use Illuminate\Support\Facades\Route;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    $user = \App\Employer::all();
+    return view('welcome', compact('user'));
+});
+/*=======================Поиск работы==================================*/
+
+//Еще не реализован, для поиска работы
+Route::group(['prefix' => 'aspirant', 'namespace' => 'Aspirant',
+    'middleware' => ['auth']], function () {
+    Route::get('/', function () {//newdiplom/aspirant
+        return view('aspirant/aspirant');
+    });
+}
+);
+/*=====================================================================*/
+
+/*=======================Поиск сторудника==============================*/
+//Еще не реализован, для работодателя
+Route::group(['prefix' => 'employer', 'namespace' => 'Employer',
+    'middleware' => ['auth']], function () {
+    Route::get('/', function () {//newdiplom/aspirant
+        return view('employer/employer');
+    });
+}
+);
+
+/*==============================Оферы==================================*/
+//Route::get('/offers', 'Offers\OfferController@offer');
+Route::group(['prefix' => 'offers', 'namespace' => 'Оffers',
+    'middleware' => ['auth']], function () {
+    Route::get('/', function () {//newdiplom/aspirant
+        $r = Vacancy::all();
+        return view('offer/offer',compact('r'));
+    });
+}
+);
+/*=====================================================================*/
+
+/*==================Админ панель=======================================*/
+Route::get('admin', 'Admin\AdminController@index');
+Route::resource('admin/roles', 'Admin\RolesController');
+Route::resource('admin/permissions', 'Admin\PermissionsController');
+Route::resource('admin/users', 'Admin\UsersController');
+Route::resource('admin/pages', 'Admin\PagesController');
+Route::resource('admin/activitylogs', 'Admin\ActivityLogsController')->only([
+    'index', 'show', 'destroy'
+]);
+Route::resource('admin/settings', 'Admin\SettingsController');
+Route::get('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@getGenerator']);
+Route::post('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@postGenerator']);
+/*=====================================================================*/
 
 Auth::routes();
 
